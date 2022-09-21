f6ece40bd0dcf7d50a11ce41b254f34a0fa9e211
PeterHolmqvist <peterholmqvist@hotmail.com>
2022-09-21 13:16:58 +0000

    add math functions

--- assets/script/sript.js
+++ assets/script/sript.js
@@ -1,5 +1,20 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit"){
+                alert("you clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`you clicked ${gameType}`);
+            }
+        })
+    }
+})
+/**The main game function called onload documnet */
 function runGame() {
-
+let num1 = Math.floor(Math.random() * 25) + 1;
+let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
@@ -31,5 +46,5 @@ function displayMultiplyQuestion() {
 }
 
 function displayDivideQuestion() {
-    
+
 }
